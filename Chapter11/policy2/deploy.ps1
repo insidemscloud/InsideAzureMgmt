f6a45eb616b3d9c8d33b63ba21ace304884345d1
Stanislav Zhelyazkov <stanislav.zhelyazkov@sentia.dk>
2020-02-11 17:50:45 +0200

    adds policy 2

---
+++ Chapter11/policy2/deploy.ps1
@@ -0,0 +1,48 @@
+param
+(
+    [Parameter (Mandatory=$true)]
+    [string] $SubscriptionId,
+
+    [Parameter (Mandatory=$false)]
+    [string] $TenantId
+)
+
+
+if(Get-AzContext | Where-Object {$_.Subscription.Id -eq $SubscriptionId})
+{
+    $selectSubscriptionParams = @{
+        SubscriptionId = $SubscriptionId
+        ErrorAction = 'Stop'
+    }
+    if($TenantId)
+    {
+        $selectSubscriptionParams.Add('TenantId', $TenantId)
+    }
+    Select-AzSubscription @selectSubscriptionParams
+}
+else
+{
+    Connect-AzAccount -ErrorAction Stop
+
+    $selectSubscriptionParams = @{
+        SubscriptionId = $SubscriptionId
+        ErrorAction = 'Stop'
+    }
+    if($TenantId)
+    {
+        $selectSubscriptionParams.Add('TenantId', $TenantId)
+    }
+
+    Select-AzSubscription @selectSubscriptionParams
+}
+
+$firstDeploymentParams = @{
+    Name = 'azDeploy-' + ((Get-Date).ToUniversalTime() ).ToString('yyMMdd-HHmm')
+    Location = 'West Europe'
+    TemplateFile = '.\azuredeploy.json'
+    TemplateParameterFile = '.\azuredeploy.parameters.json'
+    OutVariable = 'firstDeployment'
+    Verbose = $true
+    ErrorAction = 'Stop'
+}
+New-AzDeployment @firstDeploymentParams
